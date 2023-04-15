fcf52bf84b712784715a5bcd928a773f2c12112e
fdiana123 <123403220+fdiana123@users.noreply.github.com>
2023-04-15 14:53:42 +0300

    first commit

---
+++ test2.js
@@ -0,0 +1,9 @@
+const headers = document.querySelectorAll("[data-name='accordeon-title']");
+
+
+headers.forEach(function (item){
+    item.addEventListener('click', function () {
+        this.nextElementSibling.classList.toggle('hidden');
+
+    })
+})
